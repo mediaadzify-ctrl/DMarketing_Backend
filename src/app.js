a785e2af6a2b65644e9d04839ea53c5376ff5d59
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2026-02-11 23:40:40 +0530

    added cors url

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://d-marketing.vercel.app"
+    "https://d-marketing.vercel.app",
+    "https://adzifymedia.com",
   ],
   credentials: true
 }));
